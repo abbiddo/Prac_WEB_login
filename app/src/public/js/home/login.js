5605bd4c731f89f4ba8cd79de4f61155a43daa60
abbiddo@naver.com <abbiddo@naver.com>
2022-02-19 00:48:04 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -14,4 +14,12 @@ function login(){
         id: id.value,
         psword: psword.value,
     };
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req)
+    })
 }
