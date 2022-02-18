917427274e2899f3f885cb7b546f0d4db68f81a4
abbiddo@naver.com <abbiddo@naver.com>
2022-02-19 00:41:44 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+//DOM -> Document Object Model
+const id = document.querySelector("#id"),
+    password = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+console.log(id);
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req={
+        id: id.value,
+        psword: psword.value,
+    };
+}
